2755a59dc5dc2d2c204c464bd3eded1a94594ec0
SeymaCatikkasli <seymactk1@gmail.com>
2023-08-17 17:05:47 +0300

    to-do-app-with-sqlite

--- TO-DO PROJECT/ui/data/repo/ToDoDaoRepository.swift
+++ TO-DO PROJECT/ui/data/repo/ToDoDaoRepository.swift
@@ -11,30 +11,93 @@ import RxSwift
 class ToDoDaoRepository {
     var toDoListesi = BehaviorSubject<[ToDo]>(value: [ToDo]())
     
+    let db:FMDatabase?
+    
+    init() {
+        let dosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let veritabaniUrl = URL(fileURLWithPath: dosyaYolu).appendingPathComponent("toDos2.sqlite")
+        db = FMDatabase(path: veritabaniUrl.path)
+    }
     func kaydet(to_do_ad:String)  {
-        print("To-Do Kaydet : \(to_do_ad)")
+        db?.open()
+        do{
+            try db!.executeUpdate("INSERT INTO toDos (yapilacak_adi) VALUES (?)", values: [to_do_ad])
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
+
     }
     
     
     func guncelle(to_do_id:Int, to_do_ad:String){
-        print("To-Do Güncelle : \(to_do_id) - \(to_do_ad)")
+        db?.open()
+        do{
+            try db!.executeUpdate("UPDATE toDos SET yapilacak_adi = ?  WHERE yapilacak_id = ?", values: [to_do_ad,to_do_id])
+        }catch {
+            print(error.localizedDescription)
+        }
         
+        db?.close()
     }
     
     func sil(to_do_id:Int){
-        print("TO-DO sil : \(to_do_id)")
-    }
+        
+        db?.open()
+        do{
+            try db!.executeUpdate("DELETE FROM toDos WHERE yapilacak_id = ?", values: [to_do_id])
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        db?.close()    }
+    
     func ara(aramaKelimesi:String){
-        print("TO-DO ara : \(aramaKelimesi)")
-    }
-    func toDoYukle(){
+        
+        db?.open()
         var liste = [ToDo]()
-        let t1 = ToDo(to_do_id: 1, to_do_ad: "Ödev Yapılacak")
-        let t2 = ToDo(to_do_id: 2, to_do_ad: "Alışveriş Yapılacak")
-        let t3 = ToDo(to_do_id: 3, to_do_ad: "Cilt Bakımı Yapılacak")
-        liste.append(t1)
-        liste.append(t2)
-        liste.append(t3)
-        toDoListesi.onNext(liste)
+
+        do{
+            let result = try db!.executeQuery("SELECT * FROM toDos WHERE yapilacak_adi like  '%\(aramaKelimesi)%'", values: nil)
+            
+            while result.next() {
+                let yapilacak_id = Int(result.string(forColumn: "yapilacak_id"))!
+                let yapilacak_adi = result.string(forColumn: "yapilacak_adi")!
+                let toDo = ToDo(to_do_id: yapilacak_id, to_do_ad: yapilacak_adi)
+                liste.append(toDo)
+            }
+             toDoListesi.onNext(liste)
+
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
+    }
+    
+    func toDoYukle(){
+        
+        db?.open()
+        var liste = [ToDo]()
+
+        do{
+            let result = try db!.executeQuery("SELECT * FROM toDos", values: nil)
+            
+            while result.next() {
+                let yapilacak_id = Int(result.string(forColumn: "yapilacak_id"))!
+                let yapilacak_adi = result.string(forColumn: "yapilacak_adi")!
+                let toDo = ToDo(to_do_id: yapilacak_id, to_do_ad: yapilacak_adi)
+                liste.append(toDo)
+            }
+             toDoListesi.onNext(liste)
+
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
+        
+        
     }
 }
--- TO-DO PROJECT/ui/viewmodel/ViewControllerViewModel.swift
+++ TO-DO PROJECT/ui/viewmodel/ViewControllerViewModel.swift
@@ -13,16 +13,36 @@ class ViewControllerViewModel{
     var toDoListesi = BehaviorSubject<[ToDo]>(value: [ToDo]())
     
     init(){
+        veritabaniKopyala()
         toDoListesi = krepo.toDoListesi
     }
     func sil(to_do_id:Int){
         krepo.sil(to_do_id: to_do_id)
+        toDoYukle()
     }
     func ara(aramaKelimesi:String){
         krepo.ara(aramaKelimesi: aramaKelimesi)
     }
     func toDoYukle(){
         krepo.toDoYukle()
+    }
+    func veritabaniKopyala(){
+        let bundleYolu = Bundle.main.path(forResource: "toDos2", ofType: ".sqlite")
+        
+        let dosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        let veritabaniUrl = URL(fileURLWithPath: dosyaYolu).appendingPathComponent("toDos2.sqlite")
+        let fm  = FileManager.default
+        if fm.fileExists(atPath: veritabaniUrl.path){
+            print("Veritabanı zaten var")
+        }else{
+            do{
+                try fm.copyItem(atPath: bundleYolu!, toPath: veritabaniUrl.path)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
     }
     }
     
